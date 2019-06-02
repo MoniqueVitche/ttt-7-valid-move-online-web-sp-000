22b90d409cecb427493001eb03b34e807a4721e9
Monique Vitche <mvitche7@gmail.com>
2019-06-02 15:47:43 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -4,6 +4,8 @@ def valid_move?(board, position)
       true
     end
   end
+
+    # position.to_i.between?(1,9) && !position_taken?(board, position.to_i-1)
 end
 
 def position_taken?(board, position)
